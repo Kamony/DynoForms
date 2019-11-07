27a74c63f523ea40e84e9aa88e2f74b1720dfb43
Jirka H <jirihauser94@gmail.com>
2019-11-07 18:31:05 +0100

    Added Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 
 const App: React.FC = () => {
-    return <div>Clean React Template</div>;
+    return (
+        <>
+            <Typography align={'center'} variant={'h1'}>
+                Clean React Template
+            </Typography>
+            <AccessAlarmIcon />
+        </>
+    );
 };
 
 export default App;
