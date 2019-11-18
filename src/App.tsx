3c1d54485862382b2bc8987bcaa27aadf69ef1bb
Jirka H <jirihauser94@gmail.com>
2019-11-18 14:30:42 +0100

    Store and theme seed

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Box, Container, CssBaseline, Typography } from '@material-ui/core';
+import { Box, Container, createMuiTheme, CssBaseline, Typography } from '@material-ui/core';
 import { DropArea } from './containers/drop-area/DropArea';
 import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { FieldBox } from './containers/field-box/FieldBox';
+import { DataViewer } from './containers/dataViewer';
+import { ThemeProvider, DefaultTheme } from '@material-ui/styles';
+
+const theme = createMuiTheme();
 
 const App: React.FC = () => {
     return (
         <DndProvider backend={HTML5Backend}>
-            <CssBaseline />
-            <Container>
-                <Typography align={'center'} variant={'h2'} color={'secondary'} gutterBottom>
-                    DynoForms
-                </Typography>
-                <Box display="flex" flexDirection={'row'} justifyContent={'space-around'}>
-                    <FieldBox />
-                    <DropArea />
-                </Box>
-            </Container>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Container>
+                    <Typography align={'center'} variant={'h2'} color={'secondary'} gutterBottom>
+                        DynoForms
+                    </Typography>
+                    <Box display="flex" flexDirection={'row'} justifyContent={'space-around'}>
+                        <FieldBox />
+                        <DropArea />
+                        <DataViewer />
+                    </Box>
+                </Container>
+            </ThemeProvider>
         </DndProvider>
     );
 };
---
+++ src/containers/dataViewer.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useStore } from '../store/FormStore';
+import { makeStyles, useTheme } from '@material-ui/styles';
+import { Box, Typography } from '@material-ui/core';
+
+type Props = {};
+
+const useStyles = makeStyles((theme: any) => {
+    console.log(theme);
+    return {
+        root: {
+            backgroundColor: theme.palette.primary.dark,
+            color: theme.palette.primary.contrastText,
+            display: 'flex',
+            flexGrow: 1,
+            maxWidth: 300,
+            overflow: 'scroll',
+        },
+    };
+});
+
+export const DataViewer: React.FC<Props> = (props: Props) => {
+    const classes = useStyles();
+    const [elements] = useStore(s => s.elements);
+    const theme = useTheme();
+    console.log(theme);
+    return (
+        <Box display={'flex'} flexDirection={'column'}>
+            <Typography variant={'h5'} color={'primary'} gutterBottom>
+                Data view
+            </Typography>
+            <pre className={classes.root}>{JSON.stringify(elements, null, 2)}</pre>
+        </Box>
+    );
+};
